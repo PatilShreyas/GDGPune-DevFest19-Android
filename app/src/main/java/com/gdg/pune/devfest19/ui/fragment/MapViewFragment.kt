b005609cd08028d64ed96c9bb56c10bff84e7826
Shreyas Patil <shreyaspatilg@gmail.com>
2019-07-28 17:53:24 +0530

    Added Empty Fragments for implementation

---
+++ app/src/main/java/com/gdg/pune/devfest19/ui/fragment/MapViewFragment.kt
@@ -0,0 +1,26 @@
+package com.gdg.pune.devfest19.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gdg.pune.devfest19.R
+
+
+class MapViewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+
+}
