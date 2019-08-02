6ecb4c37797f841bc4c3b8ba27893f10aa77e31d
Shreyas Patil <shreyaspatilg@gmail.com>
2019-08-02 16:14:02 +0530

    Added SessionDetailsActivity, SpeakerDetailsActivity

---
+++ app/src/main/java/com/gdg/pune/devfest19/ui/SpeakerDetailsActivity.kt
@@ -0,0 +1,19 @@
+package com.gdg.pune.devfest19.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gdg.pune.devfest19.R
+
+class SpeakerDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speaker_details)
+
+        // TODO: implement
+    }
+
+    companion object {
+        const val SPEAKER_KEY = "speaker_key"
+    }
+}
